a82afb8243e4aef29d9a99b5c2fc0be64b279ce3
Christian Emeka <54124037+sonofnos@users.noreply.github.com>
2024-11-24 14:33:59 +0100

    Add localization support and update project configurations

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/appwrite.dart' as appwrite;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:revolutionary_stuff/l10n/l10n.dart';
 import 'package:revolutionary_stuff/router.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   // await SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
-  Client client = Client();
+  appwrite.Client client = appwrite.Client();
   client.setProject('6742d23d00333f806b41');
 
   runApp(
@@ -26,7 +29,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'Flutter Demo',
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        DefaultMaterialLocalizations.delegate,
+        DefaultCupertinoLocalizations.delegate,
+        DefaultWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: L10n.all,
+      locale: L10n.all[0],
+      title: AppLocalizations.of(context)!.appName,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
