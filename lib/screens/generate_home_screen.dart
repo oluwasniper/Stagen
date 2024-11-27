9a5bbe43d4835de1078a22e10fee40f5c3471995
Christian Emeka <54124037+sonofnos@users.noreply.github.com>
2024-11-27 14:51:18 +0100

    Add new screen implementations for home, history, onboarding, and scan

---
+++ lib/screens/generate_home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class GenerateHomeScreen extends StatelessWidget {
+  const GenerateHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screens/onboarding.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
