6502f1fc28427026e459d480c088c0cff93182f3
Christian Emeka <54124037+sonofnos@users.noreply.github.com>
2024-11-27 14:50:56 +0100

    Disable debug banner in the main application

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       localizationsDelegates: const [
         AppLocalizations.delegate,
         DefaultMaterialLocalizations.delegate,
