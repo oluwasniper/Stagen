94cd6e7dd35a7ac0ce55491b18f112902eed89e5
Christian Emeka <54124037+sonofnos@users.noreply.github.com>
2024-12-01 16:59:15 +0100

    Add SVG icons and update asset paths in AppAsset class

--- lib/utils/app_asset.dart
+++ lib/utils/app_asset.dart
@@ -1,5 +1,9 @@
 class AppAsset {
   const AppAsset._();
 
-  static const String icon = 'assets/icon/icon.png';
+  static const String icon = 'assets/icons/icon.png';
+
+  static const String generateIconSvg = 'assets/svgs/generateIcon.svg';
+  static const String scanIconSvg = 'assets/svgs/scanIcon.svg';
+  static const String historyIconSvg = 'assets/svgs/historyIcon.svg';
 }
