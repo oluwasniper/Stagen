2e34273bcb545fb63a6d039b0a8361b228f17e0b
Christian Emeka <54124037+sonofnos@users.noreply.github.com>
2024-12-01 18:09:47 +0100

    wip: Refactor HistoryScreen to use a TabBar and implement a new layout with custom styling

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -1,10 +1,145 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:developer';
 
-class HistoryScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:iconify_flutter/iconify_flutter.dart';
+import 'package:iconify_flutter/icons/ri.dart';
+import 'package:revolutionary_stuff/utils/app_router.dart';
+import 'package:revolutionary_stuff/utils/route/app_path.dart';
+
+class HistoryScreen extends StatefulWidget {
   const HistoryScreen({super.key});
 
   @override
+  State<HistoryScreen> createState() => _HistoryScreenState();
+}
+
+class _HistoryScreenState extends State<HistoryScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 2, vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color(0xff3D3D3D),
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: Text(
+            'History',
+            style: TextStyle(
+              fontSize: 27,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {
+              AppGoRouter.router.push(AppPath.settings);
+            },
+            child: Padding(
+              padding: EdgeInsets.only(
+                right: 20,
+              ),
+              child: Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    // color: Color(0xffFDB623),
+                    color: Color(0xff333333)),
+                child: Iconify(
+                  Ri.menu_3_line,
+                  size: 30,
+                  color: Color(0xffFDB623),
+                ),
+              ),
+            ),
+          ),
+        ],
+        backgroundColor: Colors.transparent,
+        centerTitle: false,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            // give the tab bar a height [can change hheight to preferred height]
+            Container(
+              height: 45,
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.circular(
+                  25.0,
+                ),
+              ),
+              child: TabBar(
+                controller: _tabController,
+                // give the indicator a decoration (color and border radius)
+                indicator: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    25.0,
+                  ),
+                  color: Colors.green,
+                ),
+                labelColor: Colors.white,
+                unselectedLabelColor: Colors.black,
+                tabs: [
+                  // first tab [you can add an icon using the icon property]
+                  Tab(
+                    text: 'Place Bid',
+                  ),
+
+                  // second tab [you can add an icon using the icon property]
+                  Tab(
+                    text: 'Buy Now',
+                  ),
+                ],
+              ),
+            ),
+            // tab bar view here
+            Expanded(
+              child: TabBarView(
+                controller: _tabController,
+                children: [
+                  // first tab bar view widget
+                  Center(
+                    child: Text(
+                      'Place Bid',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+
+                  // second tab bar view widget
+                  Center(
+                    child: Text(
+                      'Buy Now',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
