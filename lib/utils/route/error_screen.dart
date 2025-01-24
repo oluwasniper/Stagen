e90847c5a1351256654d6ae69493457d8aa6203e
Christian Emeka <54124037+sonofnos@users.noreply.github.com>
2025-01-24 18:18:19 +0100

    Add localized error messages and "Go Back" button to error screen

--- lib/utils/route/error_screen.dart
+++ lib/utils/route/error_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:revolutionary_stuff/utils/route/app_path.dart';
 
 import '../app_router.dart';
 
@@ -13,19 +14,19 @@ class ErrorScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Error: Page not found'),
+            Text(AppLocalizations.of(context)!.errorText),
             ElevatedButton(
               onPressed: () {
-                AppGoRouter.router.go('/onboarding');
+                AppGoRouter.router.go(AppPath.splash);
               },
               child: Text(AppLocalizations.of(context)!.onboardingHeader),
             ),
-            Text('OR'),
+            Text(AppLocalizations.of(context)!.errorOR),
             ElevatedButton(
               onPressed: () {
                 AppGoRouter.router.pop();
               },
-              child: Text("Go Back"),
+              child: Text(AppLocalizations.of(context)!.errorGoBack),
             ),
           ],
         ),
