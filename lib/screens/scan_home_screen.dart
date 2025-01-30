6cafdb2c43bdc3a4341dc48117acb94d06e9283b
Christian Emeka <54124037+sonofnos@users.noreply.github.com>
2025-01-30 10:52:28 +0100

    Implement QR code scanning functionality in ScanHomeScreen

--- lib/screens/scan_home_screen.dart
+++ lib/screens/scan_home_screen.dart
@@ -1,10 +1,59 @@
-import 'package:flutter/cupertino.dart';
 
-class ScanHomeScreen extends StatelessWidget {
+import 'package:flutter/cupertino.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+class ScanHomeScreen extends StatefulWidget {
   const ScanHomeScreen({super.key});
 
   @override
+  State<ScanHomeScreen> createState() => _ScanHomeScreenState();
+}
+
+class _ScanHomeScreenState extends State<ScanHomeScreen> {
+  final MobileScannerController controller = MobileScannerController();
+  String? qrScan;
+  bool redirect = false;
+  final double scanArea = 300.0; // Define the scanArea variable
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Stack(
+      children: [
+        // MobileScanner(
+        //   onDetect: (capture) {
+        //     final List<Barcode> barcodes = capture.barcodes;
+        //     for (final barcode in barcodes) {
+        //       if (barcode.rawValue != null) {
+        //         log(barcode.rawValue!);
+        //       } else {
+        //         log('Barcode value is null');
+        //       }
+        //     }
+        //   },
+        // )
+
+        MobileScanner(
+          controller: controller,
+          onDetect: (BarcodeCapture capture) {
+            final List<Barcode> barcodes = capture.barcodes;
+            if (barcodes.isNotEmpty) {
+              setState(() {
+                qrScan = barcodes.first.rawValue;
+                redirect = true;
+              });
+            }
+          },
+          fit: BoxFit.contain,
+          scanWindow: Rect.fromCenter(
+            center: Offset(
+              MediaQuery.of(context).size.width / 2,
+              MediaQuery.of(context).size.height / 2,
+            ),
+            width: scanArea,
+            height: scanArea,
+          ),
+        ),
+      ],
+    );
   }
 }
