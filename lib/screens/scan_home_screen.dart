1c133d0ca720ff0f2c57eb1d7d4d2b5dacf8427a
Christian Emeka <christian1emeka@gmail.com>
2026-03-01 21:19:37 +0100

    feat(scan): persist scans and add result screens
    Save scanned data to providers/Appwrite and implement generated/scanned result UIs with copy/share/open actions.

--- lib/screens/scan_home_screen.dart
+++ lib/screens/scan_home_screen.dart
@@ -1,23 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
-class ScanHomeScreen extends StatefulWidget {
+import '../models/qr_record.dart';
+import '../providers/qr_providers.dart';
+import '../utils/app_router.dart';
+import '../utils/route/app_path.dart';
+
+class ScanHomeScreen extends ConsumerStatefulWidget {
   const ScanHomeScreen({super.key});
 
   @override
-  State<ScanHomeScreen> createState() => _ScanHomeScreenState();
+  ConsumerState<ScanHomeScreen> createState() => _ScanHomeScreenState();
 }
 
-class _ScanHomeScreenState extends State<ScanHomeScreen> {
+class _ScanHomeScreenState extends ConsumerState<ScanHomeScreen> {
   final MobileScannerController controller = MobileScannerController();
   String? qrScan;
-  bool redirect = false;
-  final double scanArea = 300.0; // Define the scanArea variable
+  bool _hasNavigated = false;
+
+  void _onDetect(BarcodeCapture capture) {
+    if (_hasNavigated) return;
+    final List<Barcode> barcodes = capture.barcodes;
+    if (barcodes.isNotEmpty && barcodes.first.rawValue != null) {
+      final data = barcodes.first.rawValue!;
+      setState(() {
+        qrScan = data;
+        _hasNavigated = true;
+      });
+
+      // Store scanned data in provider
+      ref.read(scannedQRDataProvider.notifier).state = data;
+
+      // Save to Appwrite
+      final record = QRRecord(
+        data: data,
+        type: 'scanned',
+        qrType: 'scan',
+        label: 'Scanned QR',
+      );
+      ref.read(scannedHistoryProvider.notifier).addRecord(record);
+
+      // Navigate to scanned result
+      AppGoRouter.router.push(AppPath.scannedQRResult);
+    }
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
+        MobileScanner(
+          controller: controller,
+          onDetect: _onDetect,
+          fit: BoxFit.contain,
+          scanWindow: Rect.fromCenter(
+            center: Offset(
+              MediaQuery.of(context).size.width / 2,
+              MediaQuery.of(context).size.height / 2,
+            ),
+            width: MediaQuery.of(context).size.width - 60,
+            height: MediaQuery.of(context).size.width - 60,
+          ),
+        ),
         Positioned(
             top: 50,
             left: 30,
@@ -26,31 +77,37 @@ class _ScanHomeScreenState extends State<ScanHomeScreen> {
               height: 45,
               width: MediaQuery.of(context).size.width - 60,
               decoration: BoxDecoration(
-                color: Color(0xFF000000).withOpacity(0.4),
+                color: const Color(0xFF000000).withOpacity(0.4),
                 borderRadius: BorderRadius.circular(6),
               ),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   GestureDetector(
-                    onTap: () {},
-                    child: Padding(
+                    onTap: () {
+                      // TODO: pick from gallery
+                    },
+                    child: const Padding(
                       padding:
                           EdgeInsets.symmetric(vertical: 10, horizontal: 30),
                       child: Icon(Icons.photo_library_rounded),
                     ),
                   ),
                   GestureDetector(
-                    onTap: () {},
-                    child: Padding(
+                    onTap: () {
+                      controller.toggleTorch();
+                    },
+                    child: const Padding(
                       padding:
                           EdgeInsets.symmetric(vertical: 10, horizontal: 30),
                       child: Icon(Icons.flash_on_rounded),
                     ),
                   ),
                   GestureDetector(
-                    onTap: () {},
-                    child: Padding(
+                    onTap: () {
+                      controller.switchCamera();
+                    },
+                    child: const Padding(
                       padding:
                           EdgeInsets.symmetric(vertical: 10, horizontal: 30),
                       child: Icon(Icons.flip_camera_ios_rounded),
@@ -59,29 +116,6 @@ class _ScanHomeScreenState extends State<ScanHomeScreen> {
                 ],
               ),
             )),
-        MobileScanner(
-          controller: controller,
-          onDetect: (BarcodeCapture capture) {
-            final List<Barcode> barcodes = capture.barcodes;
-            if (barcodes.isNotEmpty) {
-              setState(() {
-                qrScan = barcodes.first.rawValue;
-                redirect = true;
-              });
-            }
-          },
-          fit: BoxFit.contain,
-          scanWindow: Rect.fromCenter(
-            center: Offset(
-              MediaQuery.of(context).size.width / 2,
-              MediaQuery.of(context).size.height / 2,
-            ),
-            // width: scanArea,
-            width: MediaQuery.of(context).size.width - 60,
-            // height: scanArea,
-            height: MediaQuery.of(context).size.width - 60,
-          ),
-        ),
       ],
     );
   }
